52652e5dc76e8063d5adbe224999f06eea0dbac6
Omar <ojhb1@gcloud.ua.es>
2022-02-27 23:06:31 +0100

    API REST CRUD con MongoDB

--- index.js
+++ index.js
@@ -9,41 +9,78 @@ console.log('Servidor ejecutandose en puerto 8080...');*/
 const port = process.env.PORT || 3000;
 const express = require('express');
 const logger = require('morgan');
+const mongojs = require('mongojs');
 const app = express();
+var db = mongojs("SD");
 
+var id = mongojs.ObjectID; 
 app.use(logger('dev')); // probar con: tiny, short, dev, common, combined
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.param("coleccion", (req, res, next, coleccion) => {
+    console.log('param /api/:coleccion');
+    console.log('colección: ', coleccion);
+    req.collection = db.collection(coleccion);
+    return next();
+    });
 // Implementamos el API RESTFul a través de los métodos
-app.get('/api/products', (req, res) => {
-res.status(200).send({products: [{},{},{}]});
-});
-app.get('/api/products/:productID', (req, res) => {
-    const ID = req.params.productID;
-    res.status(200).send(
-    {
-    _id: `${ID}`,
-    name:'Mesa de oficina'}
-    );
-});
-app.post('/api/products', (req, res) => {
-    const miProducto = req.body;
-    console.log(miProducto);
-    res.status(200).send({result: 'ok', product:miProducto});
-});
-app.put('/api/products/:productID', (req, res) => {
-    const miProducto = req.body;
-    const ID = req.params.productID;
-    res.status(200).send({_id:`${ID}`,
-    product:miProducto});
-});
+app.get('/api', (req, res, next) => {
+    console.log('GET /api');
+    console.log(req.params);
+     console.log(req.collection);
+     db.getCollectionNames((err, colecciones) => {
+     if (err) return next(err);
+     res.json(colecciones);
+     });
+ });
+ app.get('/api/:coleccion', (req, res, next) => {
+    req.collection.find((err, coleccion) => {
+    if (err) return next(err);
+    res.json(coleccion);
+     });
+ });
+ app.get('/api/:coleccion/:id', (req, res, next) => {
+    req.collection.findOne({_id: id(req.params.id)}, (err, elemento) => {
+    if (err) return next(err);
+    res.json(elemento);
+     });
+ });
+ app.post('/api/:coleccion', (req, res, next) => {
+     const elemento = req.body;
+     if (!elemento.nombre) {
+    res.status(400).json ({
+     error: 'Bad data',
+     description: 'Se precisa al menos un campo <nombre>'
+     });
+     } else {
+     req.collection.save(elemento, (err, coleccionGuardada) => {
+     if(err) return next(err);
+     res.json(coleccionGuardada);
+     });
+     }
+    });
+app.put('/api/:coleccion/:id', (req, res, next) => {
+        let elementoId = req.params.id;
+         let elementoNuevo = req.body;
+         req.collection.update({_id: id(elementoId)},
+         {$set: elementoNuevo}, {safe: true, multi: false}, (err, elementoModif) => {
+         if (err) return next(err);
+         res.json(elementoModif);
+         });
+         });
 app.delete('/api/products/:productID', (req, res) => {
     const ID = req.params.productID;
 res.status(200).send({result:'ok',_id: `${ID}`});
 });
 
 
-
+app.delete('/api/:coleccion/:id', (req, res, next) => {
+         let elementoId = req.params.id;
+         req.collection.remove({_id: id(elementoId)}, (err, resultado) => {
+         if (err) return next(err);
+         res.json(resultado);
+         });
+         });
 // Lanzamos nuestro servicio API
 app.listen(port, () => {
 console.log(`API REST ejecutándose en http://localhost:${port}/api/products`);
