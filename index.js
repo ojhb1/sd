354209f48139f735fedb63835ae7c6e47779d48b
Omar <ojhb1@gcloud.ua.es>
2022-03-07 20:14:49 +0100

    API REST CRUD (con seguridad)

--- index.js
+++ index.js
@@ -11,12 +11,31 @@ const express = require('express');
 const logger = require('morgan');
 const mongojs = require('mongojs');
 const app = express();
+const cors = require('cors');
 var db = mongojs("SD");
 
 var id = mongojs.ObjectID; 
+// Declaraciones
+var allowCrossTokenHeader = (req, res, next) => {
+res.header("Access-Control-Allow-Headers", "*");
+return next();
+};
+var auth = (req, res, next) => {
+    if(req.headers.token === "password1234") {
+    return next();
+    } else {
+    return next(new Error("No autorizado"));
+    };
+};
+
+// Middlewares
+app.use(cors());
 app.use(logger('dev')); // probar con: tiny, short, dev, common, combined
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(allowCrossTokenHeader);
+
+
 app.param("coleccion", (req, res, next, coleccion) => {
     console.log('param /api/:coleccion');
     console.log('colección: ', coleccion);
@@ -45,38 +64,36 @@ app.get('/api', (req, res, next) => {
     res.json(elemento);
      });
  });
- app.post('/api/:coleccion', (req, res, next) => {
-     const elemento = req.body;
-     if (!elemento.nombre) {
-    res.status(400).json ({
-     error: 'Bad data',
-     description: 'Se precisa al menos un campo <nombre>'
-     });
-     } else {
-     req.collection.save(elemento, (err, coleccionGuardada) => {
-     if(err) return next(err);
-     res.json(coleccionGuardada);
-     });
-     }
-    });
-app.put('/api/:coleccion/:id', (req, res, next) => {
-        let elementoId = req.params.id;
-         let elementoNuevo = req.body;
-         req.collection.update({_id: id(elementoId)},
-         {$set: elementoNuevo}, {safe: true, multi: false}, (err, elementoModif) => {
-         if (err) return next(err);
-         res.json(elementoModif);
-         });
-         });
 
-
-app.delete('/api/:coleccion/:id', (req, res, next) => {
-         let elementoId = req.params.id;
-         req.collection.remove({_id: id(elementoId)}, (err, resultado) => {
-         if (err) return next(err);
-         res.json(resultado);
-         });
-         });
+         app.post('/api/:coleccion', auth, (req, res, next) => {
+            const elemento = req.body;
+            if (!elemento.nombre) {
+            res.status(400).json ({
+            error: 'Bad data',
+            description: 'Se precisa al menos un campo <nombre>'
+            });
+        } else {
+            req.collection.save(elemento, (err, coleccionGuardada) => {
+            if(err) return next(err);
+            res.json(coleccionGuardada);
+            });
+            }
+            });
+        app.put('/api/:coleccion/:id', auth, (req, res, next) => {
+                let elementoId = req.params.id;
+                req.collection.update({_id: id(elementoId)}, {$set: req.body},
+                {safe: true, multi: false}, (err, result) => {
+                if (err) return next(err);
+                res.json(result);
+                });
+        });
+        app.delete('/api/:coleccion/:id', auth, (req, res, next) => {
+                let elementoId = req.params.id;
+                req.collection.remove({_id: id(elementoId)}, (err, resultado) => {
+                if (err) return next(err);
+                res.json(resultado);
+                });
+        });
 // Lanzamos nuestro servicio API
 app.listen(port, () => {
 console.log(`API REST ejecutándose en http://localhost:${port}/api/products`);
